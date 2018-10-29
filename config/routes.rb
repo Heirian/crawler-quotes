cb66d9c167b65367fcf26c488dabcdc143ea4682
Heirian <shinkcc@gmail.com>
2018-10-29 18:28:16 -0300

    Adds user token controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  # Get login token from Knock
+  post 'user_token', to: 'user_token#create'
+
   # User actions
   post '/users', to: 'users#create'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
